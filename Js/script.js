d0f56f412a84f4e32404cb5f9d1f099fa4a0f2d5
esthercate <vuthi.esther@gmail.com>
2022-03-13 22:25:05 +0300

    final commit

--- Js/script.js
+++ Js/script.js
@@ -7,9 +7,41 @@ function Contact(first, last, street, city, state) {
   this.state = state;
 }
 
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
 //User Interface or Frontend Logic 
-$(document).ready(function(){
+$(document).ready(function () {
+  $("form#new-contact").submit(function (event) {
+    event.preventDefault();
 
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
 
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedStreet, inputtedCity, inputtedState);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".street").text(newContact.street);
+      $(".city").text(newContact.city);
+      $(".state").text(newContact.state);
+    });  
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+
+  });
 
 });
