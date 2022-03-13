4a7c505e89b7621326426db7ee8821eab38b219c
esthercate <vuthi.esther@gmail.com>
2022-03-13 21:54:04 +0300

    adds form and business logic function

---
+++ Js/script.js
@@ -0,0 +1,15 @@
+//Business or Backend Logic
+function Contact(first, last, street, city, state) {
+  this.firstName = first;
+  this.lastName = last;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+//User Interface or Frontend Logic 
+$(document).ready(function(){
+
+
+
+});
