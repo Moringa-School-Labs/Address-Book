797564943931272a5e77e8d76b0ebd33e0d2ced8
esthercate <vuthi.esther@gmail.com>
2022-03-15 11:57:59 +0300

    changes on the jquery file

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,13 @@
 //Business or Backend Logic
-function Contact(first, last, street, city, state) {
+function Contact(first, last, street, city, county) {
   this.firstName = first;
   this.lastName = last;
   this.street = street;
   this.city = city;
-  this.state = state;
+  this.county = county;
 }
 
-Contact.prototype.fullName = function() {
+Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 }
 
@@ -20,27 +20,27 @@ $(document).ready(function () {
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedStreet = $("input#new-street").val();
     var inputtedCity = $("input#new-city").val();
-    var inputtedState = $("input#new-state").val();
+    var inputtedCounty = $("input#new-county").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedStreet, inputtedCity, inputtedState);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedStreet, inputtedCity, inputtedCounty);
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
-    $(".contact").last().click(function() {
+    $(".contact").last().click(function () {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
       $(".street").text(newContact.street);
       $(".city").text(newContact.city);
-      $(".state").text(newContact.state);
-    });  
+      $(".county").text(newContact.county);
+    });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-street").val("");
     $("input#new-city").val("");
-    $("input#new-state").val("");
+    $("input#new-county").val("");
 
   });
 
